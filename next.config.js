7fdd8e72ce2f19a4dcae03eb6b4d8a131cabdab4
Thomas Granbohm <thomas.granbohm@gmail.com>
2021-07-11 11:51:02 +0200

    ⚙ Updated package.json and removed storybook

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-const path = require("path");
-const { webpackFinal } = require("./.storybook/main");
-
 module.exports = {
-	webpack: (config, { storybook = false }) => {
-		webpackFinal(config, {
-			srcFolder: path.resolve(__dirname, "src"),
-		});
-
-		return config;
-	},
 	images: {
 		domains: ["i.scdn.co", "mosaic.scdn.co"],
 	},
